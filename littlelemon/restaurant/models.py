fb095495eae5be2e3e1603dd574912ae4c4e8d38
rosskholohan <ross.k.holohan@gmail.com>
2023-03-14 12:41:11 +0000

    serializers and views

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Menu(models.Model):
+class MenuItem(models.Model):
     id        = models.IntegerField(primary_key = True)
     title     = models.CharField(max_length = 255)
     price     = models.DecimalField(max_digits = 10, decimal_places = 2)
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,26 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated] 
