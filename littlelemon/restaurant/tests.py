d343a89721e7e02ac6ec180bf1b9887f052598f5
rosskholohan <ross.k.holohan@gmail.com>
2023-03-16 11:52:22 +0000

    tests

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from .models import MenuItem
 
 class MenuItemTest(TestCase):
     def test_get_item(self):
-        item = MenuItem.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(item, "IceCream : 80")
+        item = MenuItem.objects.create(id= 1,title="IceCream", price=8.00, inventory=100)
+        IceCream = MenuItem.objects.get(title="IceCream")
+        self.assertEqual(IceCream.price, 8.00)
