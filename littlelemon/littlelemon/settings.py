af4075e61136936bdfbbef352ce1e0288ffb5630
rosskholohan <ross.k.holohan@gmail.com>
2023-03-16 11:39:12 +0000

    tests

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
     "restaurant",
 ]
 
@@ -129,3 +132,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+DJOSER={"USER_ID_FIELD":"username"}
--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -25,4 +25,6 @@ router.register(r'tables', views.BookingViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurant/booking/", include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
