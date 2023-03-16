62d760425a6ada56e5a23545fbff89785e54622a
rosskholohan <ross.k.holohan@gmail.com>
2023-03-16 12:18:07 +0000

    README

---
+++ littlelemon/littlelemon/README.txt
@@ -0,0 +1,3 @@
+http://127.0.0.1:8000/
+http://localhost:8000/restaurant/booking/tables/
+http://127.0.0.1:8000/auth/token/login/
--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
-from restaurant import views
+from restaurant.views import BookingViewSet
+from restaurant import urls
 
 router = DefaultRouter(trailing_slash = False)
 
-router.register(r'tables', views.BookingViewSet)
+router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
+    path("", include(urls)),
     path("admin/", admin.site.urls),
     path("restaurant/booking/", include(router.urls)),
     path('auth/', include('djoser.urls')),
